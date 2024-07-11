2b19d9ab43327868adf400492d45cd27ff9172b1
mmraihan <mm_raihan@live.com>
2024-07-11 17:52:26 +0600

    Updated gateway routes.

--- FinTransact.Gateway/Program.cs
+++ FinTransact.Gateway/Program.cs
@@ -10,7 +10,7 @@ namespace FinTransact.Gateway
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-               .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+               .AddJsonFile("Routes/gatewayRoutes.json", optional: false, reloadOnChange: true)
                .AddEnvironmentVariables();
 
             builder.Services.AddOcelot(builder.Configuration);
